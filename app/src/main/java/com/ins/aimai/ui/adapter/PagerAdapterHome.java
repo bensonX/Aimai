09522f933629cd61e375ed4a0aa5d12c66303386
liaoinstan@qq.com <abcdefgghhh01>
2017-07-14 09:50:25 +0800

    build UI src

--- app/src/main/java/com/ins/aimai/ui/adapter/PagerAdapterHome.java
+++ app/src/main/java/com/ins/aimai/ui/adapter/PagerAdapterHome.java
@@ -3,6 +3,7 @@ package com.ins.aimai.ui.adapter;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
+import android.view.ViewGroup;
 
 import com.ins.aimai.ui.fragment.LessonFragment;
 import com.ins.aimai.ui.fragment.HomeFragment;
@@ -22,6 +23,10 @@ public class PagerAdapterHome extends FragmentPagerAdapter {
     @Override
     public int getCount() {
         return 4;
+    }
+
+    @Override
+    public void destroyItem(ViewGroup container, int position, Object object) {
     }
 
     @Override
