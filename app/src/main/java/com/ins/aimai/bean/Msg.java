f8cc5d031eda53e2f67be594d3bc487e3e8b9370
liaoinstan@qq.com <abcdefgghhh01>
2017-10-31 09:48:02 +0800

    fix msg time bugs

--- app/src/main/java/com/ins/aimai/bean/Msg.java
+++ app/src/main/java/com/ins/aimai/bean/Msg.java
@@ -66,7 +66,7 @@ public class Msg implements Serializable {
     }
 
     public String getCreateTimeBind() {
-        return TimeUtil.getTimeFor("yyyy-mm-dd HH:mm", new Date(getCreateTime()));
+        return TimeUtil.getTimeFor("yyyy-MM-dd HH:mm", new Date(getCreateTime()));
     }
 
     ///////////////////////////////////////////////
